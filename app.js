53d768d28f3ba039de3f3427bfa014914506bcd2
AlSajid <alsajiduzzaman@outlook.com>
2023-02-07 18:14:23 +0600

    cors added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 // apps
 var app = express();
@@ -12,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 
 //static file serving
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
